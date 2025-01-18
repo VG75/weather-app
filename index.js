5e7f17cdf33628e1579dc1e829c64e89640366d4
Vageesha G <vageeshag75@gmail.com>
2025-01-18 13:46:20 +0000

    I have installed all the required modules and also I have completed the html part

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Derive __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+    res.sendFile("index.html", { root: __dirname });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
