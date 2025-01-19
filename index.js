9fc3fd522c5d5fe1a437efe230cb3a0b599b1a13
Vageesha G <vageeshag75@gmail.com>
2025-01-19 10:04:49 +0000

    Now completed the first part of ejs i.e the searching part

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
-import fs from "fs";
-import path from "path";
+import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 
 // Derive __dirname for ES modules
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+const baseURL = " http://api.weatherapi.com/v1/current.json";
+const key = "441f6e2eb66949d9ab0113237251801";
 
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
+let errorPresent = false;
 
 app.get("/", (req, res) => {
-    res.render("index.ejs", { root: __dirname });
+    res.render("index.ejs");
 });
+
+app.post("/city", async (req, res) => {
+    try {
+        const result = await axios.get(baseURL, {
+            params: {
+                q: req.body.city,
+                key: key,
+                aqi: "no"
+            }
+        });
+        
+       
+      } catch (error) {
+        let errorMessage = error.response.data.error.message;
+        res.render("index.ejs", {error: errorMessage});
+        res.status(500);
+      }
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
